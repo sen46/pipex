9e2f904330b366ab36f772e70c92acd8d75b3825
sen46 <sw20050719@gmail.com>
2025-08-01 23:29:37 +0900

    modify

--- incs/pipex.h
+++ incs/pipex.h
@@ -13,14 +13,14 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <fcntl.h>
-#include "libft/libft.h"
-#include "struct.h"
-#include <stdio.h>
-#include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# include "struct.h"
+# include <stdio.h>
+# include <sys/wait.h>
 
 # define PIPES 1
 # define HERE_DOC 2
@@ -35,7 +35,6 @@ void	close_all_pipes(int cmd_count, int **pipes);
 int		initialize(int argc, char **argv, char **envp, t_pipex *pipex);
 void	get_cmd_path(t_pipex *pipex);
 void	execute_commands(t_pipex *pipex, char **envp);
-char	*ft_strjoin3(char *str1, char *str2, char *str3);
 char	*ft_strjoin3(char *str1, char *str2, char *str3);
 
 #endif
--- incs/struct.h
+++ incs/struct.h
@@ -13,31 +13,14 @@
 #ifndef STRUCT_H
 # define STRUCT_H
 
-/*
-typedef struct s_file
-{
-	int	f1;
-	int	f2;
-	int	type;
-	char	*path;
-} t_file;
-*/
-
 typedef struct s_pipex
 {
-	// validateで設定
 	int		infile_fd;
 	int		outfile_fd;
 	int		type;
-	// int		**pipes;
-
-	// initializeで設定
-	char	**paths;      // 解析済みのPATH
-	char	***cmd_args;  // コマンド引数配列、何番目もコマンド、コマンドの文字列の集合をダブルポインタで管理する
-						   // コマンドはオプションがついている場合文字列の集合に分ける必要があるため
-	// コマンド数
+	char	**paths;
+	char	***cmd_args;
 	int		cmd_count;
-	// フルパス
 	char	**cmd_path;
 }	t_pipex;
 
@@ -45,5 +28,6 @@ typedef struct s_fd
 {
 	int	pipe[2];
 	int	prev;
-} t_fd;
+}	t_fd;
+
 #endif
